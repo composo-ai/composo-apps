2e61775a8dc2c034fc5c67eaaa1b3a86a5f56c80
composo GitHub Action <action@github.com>
2023-12-13 21:55:44 +0000

    Merge pull request #154 from composo-ai/test
    Test

--- financial_qna/document_qna_composo.py
+++ financial_qna/document_qna_composo.py
@@ -1,3 +1,4 @@
+
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
--- medical/main.py
+++ medical/main.py
@@ -1,3 +1,4 @@
+
 import composo as cp
 import openai
 
