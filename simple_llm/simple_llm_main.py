f0f84cf5f266c00143b3e9379e5676335e516dfa
composo GitHub Action <action@github.com>
2024-01-18 12:58:24 +0000

    Merge pull request #205 from composo-ai/dev
    database migration

--- simple_llm/simple_llm_main.py
+++ simple_llm/simple_llm_main.py
@@ -3,7 +3,7 @@ import copy
 from litellm import completion
 import litellm
 
-litellm.set_verbose = True
+# litellm.set_verbose = True
 
 model_mapping = {
     "OpenAI GPT-3.5 Turbo": "gpt-3.5-turbo",
@@ -26,29 +26,29 @@ def simple_llm_call(
     temperature: cp.FloatParam(min=0.0, max=2.0),
     system_message: cp.StrParam(description="System Message."),
     conversation_history: cp.ConversationHistoryParam,
-):
+) -> litellm.utils.CustomStreamWrapper:
     messages = copy.deepcopy(conversation_history)
     messages.insert(0, {"role": "system", "content": system_message})
 
-    prediction = (
-        completion(
-            model_mapping[model], messages, temperature=temperature, max_tokens=512
-        )
-        .choices[0]
-        .message["content"]
+    prediction = completion(
+        model_mapping[model],
+        messages,
+        temperature=temperature,
+        max_tokens=512,
+        stream=True,
     )
 
     return prediction
 
 
-output = simple_llm_call(
+stream = simple_llm_call(
     model="OpenAI GPT-3.5 Turbo",
     temperature=0.7,
     system_message="Be kind and helpful",
     conversation_history=[
         {
             "role": "user",
-            "content": "Hi! What's good?",
+            "content": "write a poem about a llama",
         }
     ],
 )
