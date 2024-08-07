03c311b0a485196d1ea01ae4e75d38ffc0f2e421
composo GitHub Action <action@github.com>
2024-08-07 13:01:13 +0000

    Merge pull request #312 from composo-ai/dev
    updating pnpm workflow

--- python/hello_world/main.py
+++ python/hello_world/main.py
@@ -8,4 +8,20 @@ def hello_world(
     return f"Hello, {name}!"
 
 
-hello_world("Armin")
+hello_world("Seb")
+
+
+
+# \begin{minipage}{\linewidth}
+# \begin{lstlisting}
+# import composo as cp
+
+# @cp.Composo.link(development_mode=True)
+# def hello_world(
+#     name: cp.StrParam,
+# ):
+#     return f"Hello, {name}!"
+
+# hello_world("Seb")
+# \end{lstlisting}
+# \end{minipage}
